91b79ccfe515ce8bb711c2f1a6613bd3e24ea6eb
Prakhar Shukla <pshukla650@gmail.com>
2025-12-16 00:37:29 +0530

    Fix: Wildcard route handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ if (ENV.NODE_ENV === "production") {
 
   app.use(express.static(distPath));
 
-  app.get("*", (req, res) => {
+  app.get("/:path(.*)", (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
